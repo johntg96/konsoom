bb1f0512d6695bb3a0b593d80bf829a6d868f2f9
John Garrison <splendidonkey@gmail.com>
2023-07-14 11:02:44 -0500

    url parameters added to url of media type and search input after search button click

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,26 @@
-console.log(`connected`);
+const searchBox = $(`#search-box`);
+const searchBoxBtn = $(`#search-box-btn`);
+
+const omdpApiRootUrl = `http://www.omdbapi.com/?apikey=[yourkey]&`;
+
+const handleFormSubmit = () => {
+  let search = searchBox.val();
+  let mediaType;
+
+  function checkBoxCheck() {
+    const radios = document.getElementsByName('media-type');
+    for (let i = 0; i < radios.length; i++) {
+      if (radios[i].checked) {
+        mediaType = radios[i].value;
+        return mediaType;
+      }
+    }
+  }
+
+  document.location.href = `?${checkBoxCheck()}=${search}`; // <- ?key=value // <- URL Set Parameters
+}
+
+searchBoxBtn.on(`click`, function(event) {
+  event.preventDefault();
+  handleFormSubmit();
+});
