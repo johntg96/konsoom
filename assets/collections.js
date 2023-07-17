6a2f6a8a67f51aeed3c6b6ef5ee41bcc89b0e639
John Garrison <splendidonkey@gmail.com>
2023-07-17 10:44:18 -0500

    added 'All' radio button to search for all types of media from api

--- assets/collections.js
+++ assets/collections.js
@@ -2,6 +2,7 @@
 const omdpApiKey = `ab8ac5ee`;
 const omdpApiRootUrl = `http://www.omdbapi.com/?apikey=${omdpApiKey}&`;
 const urlParamsStr = document.location.href;
+const mediaTypeCheckboxAllTypes = $(`#all-types`);
 const mediaTypeCheckboxMovie = $(`#movie`);
 const mediaTypeCheckboxSeries = $(`#series`);
 const mediaTypeCheckboxEpisode = $(`#episode`);
@@ -30,22 +31,32 @@ function getUrlParam(paramName){
 function setCheckBoxes() {
   // set 'checked' property of radio button
   switch (mediaType) {
+    case (`all-types`):
+      mediaTypeCheckboxAllTypes.prop("checked", true);
+      mediaTypeCheckboxMovie.prop("checked", false);
+      mediaTypeCheckboxSeries.prop("checked", false);
+      mediaTypeCheckboxEpisode.prop("checked", false);
+      break;
     case (`movie`):
+      mediaTypeCheckboxAllTypes.prop("checked", false);
       mediaTypeCheckboxMovie.prop("checked", true);
       mediaTypeCheckboxSeries.prop("checked", false);
       mediaTypeCheckboxEpisode.prop("checked", false);
       break;
     case (`series`):
+      mediaTypeCheckboxAllTypes.prop("checked", false);
       mediaTypeCheckboxMovie.prop("checked", false);
       mediaTypeCheckboxSeries.prop("checked", true);
       mediaTypeCheckboxEpisode.prop("checked", false);
       break;
     case (`episode`):
+      mediaTypeCheckboxAllTypes.prop("checked", false);
       mediaTypeCheckboxMovie.prop("checked", false);
       mediaTypeCheckboxSeries.prop("checked", false);
       mediaTypeCheckboxEpisode.prop("checked", true);
       break;
     default:
+      mediaTypeCheckboxAllTypes.prop("checked", false);
       mediaTypeCheckboxMovie.prop("checked", false);
       mediaTypeCheckboxSeries.prop("checked", false);
       mediaTypeCheckboxEpisode.prop("checked", false)
@@ -73,7 +84,14 @@ function handleFormSubmit() {
 }
 
 function searchApi() {
-  let apiUrl = `${omdpApiRootUrl}t=${urlParamsArr[1][1]}&type=${mediaType}`; // also add "&type=" to URL to filter by media type (movie or tv show)
+  let apiUrl;
+
+  if (mediaType !== `all-types`) {
+    apiUrl = `${omdpApiRootUrl}s=${urlParamsArr[1][1]}&type=${mediaType}`; // also add "&type=" to URL to filter by media type (movie, series, or episode)
+  } else {
+    apiUrl = `${omdpApiRootUrl}s=${urlParamsArr[1][1]}`;
+  }
+  
   console.log(apiUrl);
 
   fetch(apiUrl)
