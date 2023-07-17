7ce9479b96baf96a94a73e77eb61338164e198d0
John Garrison <splendidonkey@gmail.com>
2023-07-17 12:02:39 -0500

    added placeholder image and styling to bootstrap cards

--- assets/search.js
+++ assets/search.js
@@ -121,19 +121,19 @@ function renderSearchResults(searchData) {
   function renderCard(poster, title, type, year) {
     console.log(poster, title, type, year);
 
+    if (poster == 'N/A') {
+      poster = `./assets/images/200x200.png`
+    }
+
     // HTML bootstrap card created dynamically here:
     searchResults.append(`
-      <div class="row">
-        <div class="col">
-          <div class="card">
-            <div class="card-header">
-              <div class="card-body">
-                <img class="card-text" src="${poster}"/>
-                <p class="card-text">${title}</p>
-                <p class="card-text">${type}</p>
-                <p class="card-text">${year}</p>
-              </div>
-            </div>
+      <div class="card mt-2">
+        <div class="card-header">
+          <div class="card-body">
+            <img class="card-img-top" src="${poster}"/>
+            <p class="card-text">${title}</p>
+            <p class="card-text">${type}</p>
+            <p class="card-text">${year}</p>
           </div>
         </div>
       </div>
