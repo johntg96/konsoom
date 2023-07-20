e2e053013f2ee29179b4578e50fff590663a5afd
dmtweedy <damon.tweedy@yahoo.com>
2023-07-20 11:16:47 -0500

    changed button text to white

--- assets/search.js
+++ assets/search.js
@@ -193,7 +193,7 @@ searchResults.on("click", ".save-btn", function () {
   const cardData = getCardData(card);
   const isInWatchlist = $(this).data("isInWatchlist") === true;
   addToWatchlist(cardData, isInWatchlist);
-  $(this).replaceWith(`<button class="btn btn-success save-btn" style="width:100%; background-color: orange; color: blue;">Added</button>`);
+  $(this).replaceWith(`<button class="btn btn-success save-btn" style="width:100%; background-color: orange; color: white;">Added</button>`);
 });
 
 $("#watchlist-results").on("click", ".remove-btn", function () {
